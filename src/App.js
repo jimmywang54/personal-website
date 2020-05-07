042e8a0622b107f2a84c4e2dfd61d2b813c70325
PinHoWang <abc8755987@gmail.com>
2020-05-06 22:12:59 -0400

    Added Images Cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Card from './Components/Card';
+
 class App extends Component {
   render() {
+    
     return (
       <div className="App">
-        <p>Personal Website</p>
+        <h2>Main Page</h2>
+        <Card />
       </div>
     );
   }
