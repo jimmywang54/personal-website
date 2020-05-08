36770145db064bda37be0892ca51b8cf1e621e69
PinHoWang <abc8755987@gmail.com>
2020-05-08 14:59:07 -0400

    Updated Portfolio Page

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import '../App.css';
+import "../App.css";
 
 import { Card, Button, Container, Row } from "react-bootstrap";
 
@@ -9,45 +9,54 @@ class Portolio extends Component {
     const projects = [
       {
         name: "Movie Reservation System",
-        imgUrl: process.env.PUBLIC_URL + "/images/movie.jpg",
+        imgUrl: process.env.PUBLIC_URL + "/images/movie.png",
         description: "",
+        webUrl: "https://github.com/PinHoWang/MovieBookingSystem",
       },
       {
         name: "Analyze Crimes in Boston",
-        imgUrl: process.env.PUBLIC_URL + "/images/crimes.jpg",
+        imgUrl: process.env.PUBLIC_URL + "/images/crimes.png",
         description: "",
+        webUrl: "https://github.com/CSYE7200/Analyze-Crimes-Boston",
       },
       {
         name: "Comments Map",
-        imgUrl: process.env.PUBLIC_URL + "/images/map.jpg",
+        imgUrl: process.env.PUBLIC_URL + "/images/map.png",
         description: "",
+        webUrl: "https://github.com/PinHoWang/Map",
       },
       {
-        name: "Coffee Shop App",
-        imgUrl: process.env.PUBLIC_URL + "/images/coffee.jpg",
+        name: "Chat Bot",
+        imgUrl: process.env.PUBLIC_URL + "/images/chatbot.png",
         description: "",
+        webUrl: "https://github.com/PinHoWang/ChatBox",
       },
+      {
+        name: "Day Care System",
+        imgUrl: process.env.PUBLIC_URL + "/images/daycare.png",
+        description: "",
+        webUrl: "https://github.com/PinHoWang/DayCareProject",
+      }
     ];
 
     return (
       <div className="portolio">
-          <Container>
-              <Row>
-              {projects.map((project) => (
-          <Card className='portfolio-card' style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={project.imgUrl} />
-            <Card.Body>
-              <Card.Title>{project.name}</Card.Title>
-              <Card.Text>
-                {project.description}
-              </Card.Text>
-              <Button variant="primary">Take a Look</Button>
-            </Card.Body>
-          </Card>
-        ))}
-              </Row>
-          </Container>
-        
+        <Container>
+          <Row className='portfolio-row'>
+            {projects.map((project) => (
+              <Card className="portfolio-card" style={{ width: "18rem" }}>
+                <Card.Img className='protfolio-card-img' variant="top" src={project.imgUrl} />
+                <Card.Body>
+                  <Card.Title>{project.name}</Card.Title>
+                  <Card.Text>{project.description}</Card.Text>
+                  <Button href={project.webUrl} target='_blank' variant="primary">
+                    Take a Look
+                  </Button>
+                </Card.Body>
+              </Card>
+            ))}
+          </Row>
+        </Container>
       </div>
     );
   }
